452c0a93129722fec2bcddd0bc238b7931c97dc1
ParraL1 <97068405+ParraL1@users.noreply.github.com>
2022-01-11 18:56:09 -0600

    ok

---
+++ main.py
@@ -0,0 +1,14 @@
+#Author: Lilliana Parra
+# Github user: ParraL1
+# Date: 01/11/2022
+# Description: Program asks user for a number of cents from 0 to 99 and outputs number of coins
+print("Please enter an amount in cents less than a dollar.")
+num_1= float(input())
+print("Your change will be:")
+print("Q:",num_1//25)
+num_1=num_1%25
+print("D:",num_1//10)
+num_1=num_1%10
+print("N:",num_1//5)
+num_1=num_1%5
+print("P:",num_1//1)
